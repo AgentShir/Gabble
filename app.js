ee60d54965d1550aa9c80edc06a95144b2682e45
Shireen Garcia <garciasal@gmail.com>
2017-08-10 14:27:24 -0700

    Configured database with name and password

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 app.use(express.static(path.join(__dirname, 'static')))
 
-// before you use this, make sure to update the default.json file in /config
-// const conn = mysql.createConnection({
-//   host: config.get('db.host'),
-//   database: config.get('db.database'),
-//   user: config.get('db.user'),
-//   password: config.get('db.password')
-// })
+const conn = mysql.createConnection({
+  host: config.get('db.host'),
+  database: config.get('db.database'),
+  user: config.get('db.user'),
+  password: config.get('db.password')
+})
 
 app.get("/", function(req, res, next){
   res.render("index", {appType:"Signup for Gabble!"})
