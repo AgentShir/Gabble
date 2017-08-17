9033a13dfb8323bf650a8427b6081a0e26913dbc
Shireen Garcia <garciasal@gmail.com>
2017-08-17 10:29:22 -0700

    Added notes because I keep mixing up post and get

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser')
 const config = require('config')
+const mysql = require('mysql')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
@@ -19,8 +20,35 @@ const conn = mysql.createConnection({
   password: config.get('db.password')
 })
 
-app.get("/", function(req, res, next){
-  res.render("index", {appType:"Signup for Gabble!"})
+//Todo: Figure out where this goes. This is the signup page for Gabble (Bruce, Max, Boots, Connor. PS - They're all dogs).
+// Todo: ADD PASSWORD TO FORM
+
+app.post("/", function(req, res, next){
+  const username = req.body.username
+  const displayname = req.body.displayname
+  const fname = req.body.fname
+  const lname = req.body.lname
+// Todo: ADD PASSWORD, I put this twice on purpose.
+
+  const sql = `
+    INSERT INTO gabblers (username, displayname, fname, lname)
+    VALUES (?, ?, ?, ?)
+  `
+
+  conn.query(sql, [username, displayname, fname, lname], function(err, results, fields){
+    if (!err) {
+      console.log('It seems to work')
+      res.redirect("/")
+    } else {
+      console.log(err)
+      res.send("Fix it!!!")
+    }
+  })
+})
+
+// This points to signup. It might. I'm writing notes because I keep mixing the damn things up.
+app.get("/signup", function(req, res, next){
+  res.render("signup", {appType:"Signup for Gabble!"})
 })
 // Todo: signup for Gabble or click to login if already a user
 
