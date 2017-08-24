efd8996da534b4dbaf6d2e03ae01110bd61258c3
Shireen Garcia <garciasal@gmail.com>
2017-08-24 15:48:20 -0700

    Things sort of work, token needed

--- app.js
+++ app.js
@@ -19,56 +19,9 @@ app.use(express.static(path.join(__dirname, 'static')))
 app.use('/', publicRoutes)
 app.use('/api', Authenticate, protectedRoutes)
 
-//Todo: Figure out where this goes. This is the signup page for Gabble (Bruce, Max, Boots, Connor, Gypsy. PS - They're all dogs).
-// Todo: ADD PASSWORD TO FORM
-
-app.post("/", function(req, res, next){
-  const displayname = req.body.displayname
-  const fname = req.body.fname
-  const lname = req.body.lname
-  const username = req.body.username
-  const password = req.body.password
-
-  const sql = `
-    INSERT INTO gabblers (displayname, fname, lname, username, password)
-    VALUES (?, ?, ?, ?, ?)
-  `
-
-  conn.query(sql, [displayname, fname, lname, username, password], function(err, results, fields){
-    if (!err) {
-      console.log('It seems to work')
-      res.redirect("/")
-    } else {
-      console.log(err)
-      res.send("Fix it!!!")
-    }
-  })
-})
-
 // Form to create new posts
 app.get("/createpost", function(req, res, next){
   res.render("createpost", {appType:"Create New Post:"})
-})
-
-// Sends new posts to posts database
-// Todo: POSTS NOT YET CONNECTED TO USERS
-app.post("/createpost", function(req, res, next){
-  const gpost = req.body.gpost
-
-  const sql2 =`
-  INSERT INTO posts(posts)
-  VALUES (?)
-  `
-
-  conn.query(sql2, [gpost], function(err, results, fields){
-    if (!err){
-      console.log('No no bad dog')
-      res.redirect("/mygabble")
-    } else{
-      console.log(err)
-      res.send("Fetch!")
-    }
-  })
 })
 
 // Signup page. Signup form.
--- routes/public.js
+++ routes/public.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const uuid = require('uuid')
+const conn = require('../lib/db');
 
 router.post("/login", function(req, res, next){
   const username = req.body.username
@@ -58,6 +59,50 @@ router.post('/register', function(req, res, next){
         token: token
       })
     })
+  })
+})
+
+router.post("/", function(req, res, next){
+  const displayname = req.body.displayname
+  const fname = req.body.fname
+  const lname = req.body.lname
+  const username = req.body.username
+  const password = req.body.password
+
+  const sql = `
+    INSERT INTO gabblers (displayname, fname, lname, username, password)
+    VALUES (?, ?, ?, ?, ?)
+  `
+
+  conn.query(sql, [displayname, fname, lname, username, password], function(err, results, fields){
+    if (!err) {
+      console.log('It seems to work')
+      res.redirect("/")
+    } else {
+      console.log(err)
+      res.send("Fix it!!!")
+    }
+  })
+})
+
+// Create post works, connects to the database, but hangs
+
+router.post("/createpost", function(req, res, next){
+  const gpost = req.body.gpost
+
+  const sql2 =`
+  INSERT INTO posts(posts)
+  VALUES (?)
+  `
+
+  conn.query(sql2, [gpost], function(err, results, fields){
+    if (!err){
+      console.log('No no bad dog')
+      res.redirect("/mygabble")
+    } else{
+      console.log(err)
+      res.send("Fetch!")
+    }
   })
 })
 
